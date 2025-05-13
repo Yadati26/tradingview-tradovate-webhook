78e043b761c794fa70ff1a6842296b67afbc89f3
Yadati26 <GabrielSrour154@gmail.com>
2025-05-13 11:46:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,8 @@ def webhook():
         print("Unknown action")
 
     return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
